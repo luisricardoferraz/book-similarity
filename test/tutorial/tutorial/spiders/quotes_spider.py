c0e364e5505823584550658824f66985068ea91f
Luis Ricardo Ferraz <Luis.Ricardo.Ferraz@ibm.com>
2018-12-26 16:11:11 -0200

    Lord of The Rings has been collected from Skoob

--- test/tutorial/tutorial/spiders/quotes_spider.py
+++ test/tutorial/tutorial/spiders/quotes_spider.py
@@ -2,18 +2,17 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.xpath('span/small/text()').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
---
+++ test/tutorial/tutorial/spiders/skoob-first-spider.py
@@ -0,0 +1,62 @@
+# Import framework to create the crawler
+import scrapy
+
+# Define crawler as a class derived from class Spider
+class SkoobSpider(scrapy.Spider):
+    
+    # Define crawler's name
+    name = 'skoob-spider'
+
+    # Define the first URL used to collect data
+    start_urls = [
+        'https://www.skoob.com.br/livro/64ED86'
+    ]
+
+    # Define the callback function parse(), executed after receiving a response of the request
+    def parse(self, response):
+        
+        # Separate data into different groups and then store them into variables
+        stats = response.css('div#livro-perfil-status b > a::text').extract()
+        rating = response.css('div#pg-livro-box-rating span.rating::text').extract_first()
+        totalAvaliacoes = response.css('div#pg-livro-box-rating-avaliadores-numero::text').extract_first()
+        maisStats = response.css('a.l_icones::text').extract()
+        titulo = response.css('.sidebar-titulo::text').extract_first()
+        autor = response.css('div#pg-livro-menu-principal-container > a::text').extract_first()
+        detalhes = response.css('div.sidebar-desc').extract_first()
+        sinopse = response.css('div#livro-perfil-sinopse-txt > p').extract_first()
+        generos = response.css('span.pg-livro-generos::text').extract_first()
+        notas = response.css('div.pg-livro-box-avaliacoes-grafico > div > div > div::text').extract()
+        tags = response.css('ul#tags > li.litags > a::text').extract()
+        avaliacoesHomens = response.css('span.pg-livro-icone-male-label::text').extract_first()
+        avaliacoesMulheres = response.css('span.pg-livro-icone-female-label::text').extract_first()
+        subtitulo = response.css('.sidebar-subtitulo::text').extract()
+
+        # Assembly new meanings for the data collected so it can be written in a .json file
+        yield {
+            'titulo': titulo,
+            'subtitulo': subtitulo,
+            'autor': autor,
+            'detalhes': detalhes,
+            'leram': stats[-1],
+            'lendo': stats[-2],
+            'queremLer': stats[-3],
+            'relendo': stats[-4],
+            'abandonos': stats[-5],
+            'resenhas': stats[-6],
+            'rating': rating,
+            'totalAvaliacoes': totalAvaliacoes,
+            'favoritos': maisStats[0],
+            'desejados': maisStats[1],
+            'trocam': maisStats[2],
+            'avaliaram': maisStats[3],
+            'sinopse': sinopse,
+            'generos': generos,
+            'cincoEstrelas': notas[0],
+            'quatroEstrelas': notas[1],
+            'tresEstrelas': notas[2],
+            'duasEstrelas': notas[3],
+            'umaEstrela': notas[4],
+            'tags': tags,
+            'avaliacoesHomens': avaliacoesHomens,
+            'avaliacoesMulheres': avaliacoesMulheres
+        }
